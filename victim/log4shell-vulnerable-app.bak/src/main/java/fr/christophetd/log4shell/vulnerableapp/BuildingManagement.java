17c551aaedbe59fad49e131570a95b21e3a0912d
Craig Koroscil <ckoroscil@circadence.com>
2022-03-24 21:46:29 -0400

    removing remote git ref

---
+++ victim/log4shell-vulnerable-app.bak/src/main/java/fr/christophetd/log4shell/vulnerableapp/BuildingManagement.java
@@ -0,0 +1,13 @@
+package fr.christophetd.log4shell.vulnerableapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingManagement {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingManagement.class, args);
+	}
+
+}
